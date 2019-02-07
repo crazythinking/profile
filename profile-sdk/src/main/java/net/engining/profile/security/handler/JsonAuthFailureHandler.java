72f6c771102f1dd565a8dbdac242acd8ff5754a6
crazythinking <crazy_thinking@163.com>
2019-02-07 23:33:19 +0800

    重构common response and request in pg-web and relation project

--- profile-sdk/src/main/java/net/engining/profile/security/handler/JsonAuthFailureHandler.java
+++ profile-sdk/src/main/java/net/engining/profile/security/handler/JsonAuthFailureHandler.java
@@ -1,46 +1,39 @@
 package net.engining.profile.security.handler;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.engining.pg.support.core.exception.ErrorCode;
+import net.engining.pg.web.CommonResponseBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import net.engining.pg.support.core.exception.ErrorCode;
-import net.engining.pg.web.BaseResponseBean;
-import net.engining.pg.web.NewWebCommonResponseBuilder;
-import net.engining.pg.web.bean.DefaultResponseHeader;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 用户身份验证失败时的处理
- * @author luxue
  *
+ * @author luxue
  */
 public class JsonAuthFailureHandler implements AuthenticationFailureHandler {
-	
-	private ObjectMapper mapper = new ObjectMapper();
 
-	@Override
-	public void onAuthenticationFailure(HttpServletRequest request,	HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-		response.setStatus(HttpStatus.UNAUTHORIZED.value());
-		
-		BaseResponseBean baseResponseBean = new BaseResponseBean();
-		baseResponseBean.setReturnCode(ErrorCode.UnknowFail.getValue());
-		baseResponseBean.setReturnDesc(ErrorCode.UnknowFail.getLabel());
-		
-		mapper.writeValue(response.getOutputStream(), 
-				new NewWebCommonResponseBuilder<DefaultResponseHeader,BaseResponseBean>()
-					.build()
-					.setStatusCode(ErrorCode.UnknowFail.getValue())
-					.setStatusDesc(ErrorCode.UnknowFail.getLabel())
-					.setResponseData(baseResponseBean)
-					);
-		
-	}
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+            throws IOException, ServletException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        mapper.writeValue(response.getOutputStream(),
+                new CommonResponseBuilder()
+                        .build()
+                        .setReturnCode(ErrorCode.Restricted.getValue())
+                        .setReturnDesc(ErrorCode.Restricted.getLabel())
+
+        );
+
+    }
 }
--- profile-sdk/src/main/java/net/engining/profile/security/handler/JsonAuthSuccessHandler.java
+++ profile-sdk/src/main/java/net/engining/profile/security/handler/JsonAuthSuccessHandler.java
@@ -1,41 +1,33 @@
 package net.engining.profile.security.handler;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.engining.pg.support.core.exception.ErrorCode;
+import net.engining.pg.web.CommonResponseBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import net.engining.pg.support.core.exception.ErrorCode;
-import net.engining.pg.web.BaseResponseBean;
-import net.engining.pg.web.NewWebCommonResponseBuilder;
-import net.engining.pg.web.bean.DefaultResponseHeader;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class JsonAuthSuccessHandler implements AuthenticationSuccessHandler {
-	private ObjectMapper mapper = new ObjectMapper();
+    private ObjectMapper mapper = new ObjectMapper();
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request,HttpServletResponse response, Authentication authentication)
-			throws IOException, ServletException {
-		
-		response.setStatus(HttpStatus.OK.value());
-		
-		BaseResponseBean baseResponseBean = new BaseResponseBean();
-		
-		mapper.writeValue(response.getOutputStream(), 
-				new NewWebCommonResponseBuilder<DefaultResponseHeader,BaseResponseBean>()
-					.build()
-					.setStatusCode(ErrorCode.Success.getValue())
-					.setStatusDesc(ErrorCode.Success.getLabel())
-					.setResponseData(baseResponseBean)
-					);
-		
-	}
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+
+        response.setStatus(HttpStatus.OK.value());
+
+        mapper.writeValue(response.getOutputStream(),
+                new CommonResponseBuilder()
+                        .build()
+                        .setReturnCode(ErrorCode.Success.getValue())
+                        .setReturnDesc(ErrorCode.Success.getLabel())
+        );
+
+    }
 
 }
