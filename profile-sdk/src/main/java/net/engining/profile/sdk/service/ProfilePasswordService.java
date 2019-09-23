76eb924f6077b6185a217eb48ba6b81c8c863b96
liudongjing <1096451424@qq.com>
2019-09-23 15:44:30 +0800

    修改密码接口

--- profile-sdk/src/main/java/net/engining/profile/sdk/service/ProfilePasswordService.java
+++ profile-sdk/src/main/java/net/engining/profile/sdk/service/ProfilePasswordService.java
@@ -3,8 +3,10 @@ package net.engining.profile.sdk.service;
 import net.engining.pg.parameter.ParameterFacility;
 import net.engining.pg.support.core.exception.ErrorCode;
 import net.engining.pg.support.core.exception.ErrorMessageException;
+import net.engining.pg.support.utils.ValidateUtilExt;
 import net.engining.profile.entity.enums.StatusDef;
 import net.engining.profile.entity.model.ProfileUser;
+import net.engining.profile.param.PasswordPattern;
 import net.engining.profile.param.SecurityControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,82 +18,107 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Date;
+import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 @Service
-public class ProfilePasswordService{
-	private Logger logger = LoggerFactory.getLogger(getClass());
+public class ProfilePasswordService {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
-	@PersistenceContext
-	private EntityManager em;
-	
-	@Autowired
-	private ParameterFacility parameterFacility;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	public final String resetPwdStr = "";
-	
-	/**
-	 * 修改登陆密码
-	 * @param puId 用户登陆表主键
-	 * @param form {@link net.engining.profile.dict.view.ChangePasswordForm}
-	 * @param operUser 操作人员系统登陆Id，可以为Null，为Null时，自动填入"System"; 为"Owner"时，表示用户自己;
-	 * @throws ErrorMessageException
-	 */
-	@Transactional
-	public void changePassword(String puId, String oldPassword, String newPassword, String operUser) throws ErrorMessageException {
-		
-		SecurityControl control = parameterFacility.getUniqueParameter(SecurityControl.class).get();
+    @PersistenceContext
+    private EntityManager em;
 
-		if (control != null && control.complexPwdInd != null && control.complexPwdInd)
-		{
-			String value = newPassword;
-			if (!value.matches(".*[a-z].*")||
-				!value.matches(".*[A-Z].*")||
-				!value.matches(".*\\d.*"))
-			{
-				throw new ErrorMessageException(ErrorCode.CheckError, "密码必须由大、小写字母及数字组成");
-			}
-		}
+    @Autowired
+    private ParameterFacility parameterFacility;
 
-		ProfileUser user = em.find(ProfileUser.class, puId);
-		checkNotNull(user);
-		
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public final String resetPwdStr = "";
+
+    private static final String HINT_PREFIX = "密码必须为";
+
+    private int complexity = 0;
+
+
+    /**
+     * 修改登陆密码
+     *
+     * @param puId     用户登陆表主键
+     * @param form     {@link net.engining.profile.dict.view.ChangePasswordForm}
+     * @param operUser 操作人员系统登陆Id，可以为Null，为Null时，自动填入"System"; 为"Owner"时，表示用户自己;
+     * @throws ErrorMessageException
+     */
+    @Transactional
+    public void changePassword(String puId, String oldPassword, String newPassword, String operUser) throws ErrorMessageException {
+
+
+        SecurityControl control = parameterFacility.getUniqueParameter(SecurityControl.class).get();
+
+        if (ValidateUtilExt.isNotNullOrEmpty(control) && control.complexPwdInd != null && control.complexPwdInd) {
+            // 根据参数决定是否校验复杂度
+            boolean isWellFormed = true;
+            StringBuilder hint = new StringBuilder(HINT_PREFIX);
+            boolean isfirstMismatch = true;
+            for (PasswordPattern pattern : control.passwordPatterns) {
+                if (!newPassword.matches(pattern.pattern)) {
+                    // 除了第一个提示短语，之后的需要先加顿号分隔
+                    if (isfirstMismatch) {
+                        isfirstMismatch = false;
+                    } else {
+                        hint.append("、");
+                    }
+                    hint.append(pattern.message);
+                    if (pattern.mustMatch) {
+                        // 密码不合规
+                        isWellFormed = false;
+                    }
+                } else {
+                    // 累积复杂度权重
+                    complexity += pattern.weights;
+                }
+            }
+            if (!isWellFormed) {
+                throw new ErrorMessageException(ErrorCode.CheckError, hint.toString());
+            }
+        }
+
+        ProfileUser user = em.find(ProfileUser.class, puId);
+        checkNotNull(user);
+
 //		UserDetails ud = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		if (!passwordEncoder.matches(oldPassword, user.getPassword()))
-		{
-			throw new ErrorMessageException(ErrorCode.CheckError,  "原密码不正确");
-		}
-		
-		user.setMtnTimestamp(new Date());
-		user.setMtnUser(operUser);
-		user.setPassword(passwordEncoder.encode(newPassword));
-		user.setPwdTries(0);
-		user.setStatus(StatusDef.A);
-		
-		logger.info("操作员{}，修改了用户{}的密码！", operUser, user.getUserId());
-	}
-	
-	/**
-	 * 重置登陆密码
-	 * @param puId
-	 * @param operUser
-	 */
-	@Transactional
-	public void resetPassword(String puId, String operUser){
-		ProfileUser user = em.find(ProfileUser.class, puId);
-		checkNotNull(user);
-		
-		user.setMtnTimestamp(new Date());
-		user.setMtnUser(operUser);
-		user.setPassword(passwordEncoder.encode(resetPwdStr));
-		user.setPwdTries(0);
-		user.setStatus(StatusDef.L);
-		
-		logger.info("操作员：{}，重置了用户：{}的密码！", operUser, user.getUserId());
-	}
+        if (!passwordEncoder.matches(oldPassword, user.getPassword())) {
+            throw new ErrorMessageException(ErrorCode.CheckError, "原密码不正确");
+        }
+
+        user.setMtnTimestamp(new Date());
+        user.setMtnUser(operUser);
+        user.setPassword(passwordEncoder.encode(newPassword));
+        user.setPwdTries(0);
+        user.setStatus(StatusDef.A);
+
+        logger.info("操作员{}，修改了用户{}的密码！", operUser, user.getUserId());
+    }
+
+    /**
+     * 重置登陆密码
+     *
+     * @param puId
+     * @param operUser
+     */
+    @Transactional
+    public void resetPassword(String puId, String operUser) {
+        ProfileUser user = em.find(ProfileUser.class, puId);
+        checkNotNull(user);
+
+        user.setMtnTimestamp(new Date());
+        user.setMtnUser(operUser);
+        user.setPassword(passwordEncoder.encode(resetPwdStr));
+        user.setPwdTries(0);
+        user.setStatus(StatusDef.L);
+
+        logger.info("操作员：{}，重置了用户：{}的密码！", operUser, user.getUserId());
+    }
 
 }
