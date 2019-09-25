fbde481a765c138c3f6fe0849a8713d026fe58c6
liudongjing <1096451424@qq.com>
2019-09-25 19:32:32 +0800

    修改密码接口

--- profile-sdk/src/main/java/net/engining/profile/sdk/service/ProfilePasswordService.java
+++ profile-sdk/src/main/java/net/engining/profile/sdk/service/ProfilePasswordService.java
@@ -37,69 +37,6 @@ public class ProfilePasswordService {
 
     public final String resetPwdStr = "";
 
-    private static final String HINT_PREFIX = "密码必须为";
-
-    private int complexity = 0;
-
-
-    /**
-     * 修改登陆密码
-     *
-     * @param puId     用户登陆表主键
-     * @param form     {@link net.engining.profile.dict.view.ChangePasswordForm}
-     * @param operUser 操作人员系统登陆Id，可以为Null，为Null时，自动填入"System"; 为"Owner"时，表示用户自己;
-     * @throws ErrorMessageException
-     */
-    @Transactional
-    public void changePassword(String puId, String oldPassword, String newPassword, String operUser) throws ErrorMessageException {
-
-
-        SecurityControl control = parameterFacility.getUniqueParameter(SecurityControl.class).get();
-
-        if (ValidateUtilExt.isNotNullOrEmpty(control) && control.complexPwdInd != null && control.complexPwdInd) {
-            // 根据参数决定是否校验复杂度
-            boolean isWellFormed = true;
-            StringBuilder hint = new StringBuilder(HINT_PREFIX);
-            boolean isfirstMismatch = true;
-            for (PasswordPattern pattern : control.passwordPatterns) {
-                if (!newPassword.matches(pattern.pattern)) {
-                    // 除了第一个提示短语，之后的需要先加顿号分隔
-                    if (isfirstMismatch) {
-                        isfirstMismatch = false;
-                    } else {
-                        hint.append("、");
-                    }
-                    hint.append(pattern.message);
-                    if (pattern.mustMatch) {
-                        // 密码不合规
-                        isWellFormed = false;
-                    }
-                } else {
-                    // 累积复杂度权重
-                    complexity += pattern.weights;
-                }
-            }
-            if (!isWellFormed) {
-                throw new ErrorMessageException(ErrorCode.CheckError, hint.toString());
-            }
-        }
-
-        ProfileUser user = em.find(ProfileUser.class, puId);
-        checkNotNull(user);
-
-//		UserDetails ud = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (!passwordEncoder.matches(oldPassword, user.getPassword())) {
-            throw new ErrorMessageException(ErrorCode.CheckError, "原密码不正确");
-        }
-
-        user.setMtnTimestamp(new Date());
-        user.setMtnUser(operUser);
-        user.setPassword(passwordEncoder.encode(newPassword));
-        user.setPwdTries(0);
-        user.setStatus(StatusDef.A);
-
-        logger.info("操作员{}，修改了用户{}的密码！", operUser, user.getUserId());
-    }
 
     /**
      * 重置登陆密码
@@ -107,7 +44,7 @@ public class ProfilePasswordService {
      * @param puId
      * @param operUser
      */
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public void resetPassword(String puId, String operUser) {
         ProfileUser user = em.find(ProfileUser.class, puId);
         checkNotNull(user);
