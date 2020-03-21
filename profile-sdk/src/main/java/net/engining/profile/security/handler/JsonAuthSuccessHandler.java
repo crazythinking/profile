cfe74926b197ff439d14c70c4cf0149212623e3e
crazythinking <crazy_thinking@163.com>
2020-03-21 16:43:37 +0800

    优化Auth相关代码

--- profile-sdk/src/main/java/net/engining/profile/security/handler/JsonAuthSuccessHandler.java
+++ profile-sdk/src/main/java/net/engining/profile/security/handler/JsonAuthSuccessHandler.java
@@ -3,6 +3,11 @@ package net.engining.profile.security.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.engining.pg.support.core.exception.ErrorCode;
 import net.engining.pg.web.CommonWithHeaderResponseBuilder;
+import net.engining.pg.web.WebCommonUtils;
+import net.engining.profile.enums.OperationType;
+import net.engining.profile.security.service.ProfileSecurityLoggerService;
+import net.engining.profile.security.service.ProfileUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -11,18 +16,34 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * 返回Json格式的spring security框架用户身份验证成功后处理
  * @author Eric Lu
  */
-@Deprecated
 public class JsonAuthSuccessHandler implements AuthenticationSuccessHandler {
-    private ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Autowired
+    ProfileSecurityLoggerService securityLoggerService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
+
+        ProfileUserDetails profileUserDetails = (ProfileUserDetails)authentication.getPrincipal();
+
+        //记录安全日志
+        securityLoggerService.logSecuOperation(
+                profileUserDetails.getPuId(),
+                OperationType.LG,
+                WebCommonUtils.getIpAddress(request),
+                new Date(),
+                null
+        );
 
         response.setStatus(HttpStatus.OK.value());
 
