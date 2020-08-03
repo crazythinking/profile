73b56d8a7d5d2838991ee4fad8506f98f9d5d534
陈宝 <chenbao@csii.com.cn>
2020-08-03 17:21:35 +0800

    切换到4.0.4.RELEASE

--- release.sh
+++ release.sh
@@ -1,33 +1,37 @@
 # 添加所有修改的内容
 git add .
 # 批量修改pom文件版本为相应RELEASE
-sed -i "s/4.0.3-SNAPSHOT/4.0.3.RELEASE/g" pom.xml */pom.xml */*/pom.xml
+sed -i "s/2.6.1-SNAPSHOT/2.6.1.RELEASE/g" pom.xml */pom.xml */*/pom.xml
+# 添加所有修改的内容
+git add .
 # 编译打包RELEASE
 mvn clean deploy
-# 创建相应版本的RELEASE tag
-git tag -a v4.0.3.RELEASE -m "4.0.3.RELEASE版本"
-# 提交tag
-git push --tags
 # 添加所有修改的内容
 git add .
 # 提交本地分支
-git commit -m '切换到4.0.3.RELEASE'
+git commit -m '切换到2.6.1.RELEASE'
 # 提交到远程分支
-git push origin sccc-4.0.3
+git push origin 2.6.1
+# 创建相应版本的RELEASE tag
+git tag -a v2.6.1.RELEASE -m "2.6.1.RELEASE版本"
+# 提交tag
+git push --tags
 #切换到master分支
 git checkout master
 # 合并目标版本到master分支
-git merge sccc-4.0.3
+git merge 2.6.1
+# 添加所有修改的内容
+git add .
 # 提交合并
 git push origin master
 # 创建切换到新创建的分支
-git checkout -b sccc-4.0.4
+git checkout -b 2.6.2
 # 将master提升版本号
-sed -i "s/4.0.3.RELEASE/4.0.4-SNAPSHOT/g" pom.xml */pom.xml */*/pom.xml
+sed -i "s/2.6.1.RELEASE/2.6.2-SNAPSHOT/g" pom.xml */pom.xml */*/pom.xml
 # 添加所有修改的内容
 git add .
 # 提交本地master分支
-git commit -m '切换到4.0.3.RELEASE'
+git commit -m '版本号升级2.6.2-SNAPSHOT'
 # 提交到远程master分支
-git push origin sccc-4.0.4
+git push origin 2.6.2
 
