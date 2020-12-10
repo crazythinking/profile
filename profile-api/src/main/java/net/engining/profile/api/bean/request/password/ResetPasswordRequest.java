b5b22db6b6f5386286aab76841b7149bb4711207
zhaoyuanmin <1264859189@qq.com>
2020-12-10 20:29:43 +0800

    重置密码

--- profile-api/src/main/java/net/engining/profile/api/bean/request/password/ResetPasswordRequest.java
+++ profile-api/src/main/java/net/engining/profile/api/bean/request/password/ResetPasswordRequest.java
@@ -23,6 +23,16 @@ public class ResetPasswordRequest extends BaseOperateRequest {
     @Length(min = 3, message = "用户ID的字段长度不能少于3个字符")
     @ApiModelProperty(value = "用户ID|3-30位的字母或数字或下划线", example = "000000000")
     private String userId;
+    /**
+     * 准入token
+     */
+    @ApiModelProperty(value = "准入token|任意字符", example = "000000000")
+    private String accessToken;
+    /**
+     * 刷新token
+     */
+    @ApiModelProperty(value = "刷新token|任意字符", example = "000000000")
+    private String refreshToken;
 
     public String getUserId() {
         return userId;
@@ -30,5 +40,21 @@ public class ResetPasswordRequest extends BaseOperateRequest {
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
     }
 }
